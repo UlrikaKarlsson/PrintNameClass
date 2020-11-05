a3a67bb1e67f0654bdef3d69dd8fc82b49756de7
Ulrika Karlsson <ulrika.karlsson@yahoo.se>
2020-11-05 21:41:32 +0100

    Add project files.

---
+++ PrintNameClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNameClass
+{
+    class Program
+    {
+        class Person
+        {
+            public string firstName, lastName, homePhone, workPhone;
+            public void Print()
+            {
+                Console.WriteLine("{0} {1} {2} {3}",
+                    firstName, lastName, homePhone, workPhone);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Person Arne = new Person()
+            {
+                firstName = "Arne",
+                lastName = "Saknusem",
+                homePhone = "013-13 13 13",
+                workPhone = "073 - 747 57 67"
+            };
+            Arne.Print();
+        }                
+    }
+}
